6fc06f47293d083cdbc3c0e80e19486dccbff742
Guilherme Martins <guimartins_9@hotmail.com>
2022-08-17 18:24:57 -0300

    feat: 4 - Adicione as validações para o endpoint /login

--- middlewares/loginValidation.js
+++ middlewares/loginValidation.js
@@ -1,12 +1,21 @@
-const emailValidation = (req, _res, next) => {
+const emailValidation = (req, res, next) => {
   const { email } = req.body;
   const emailRegex = /^\w+(\[\+\.-\]?\w)*@\w+(\[\.-\]?\w+)*\.[a-z]+$/i;
-  if (emailRegex.test(email)) return next();
+  
+  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+
+  return (!emailRegex.test(email)
+    ? res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' })
+    : next()); 
+  // if (emailRegex.test(email)) return next();
 };
 
-const passwordValidation = (req, _res, next) => {
+const passwordValidation = (req, res, next) => {
   const { password } = req.body;
-  if (password.length >= 6) return next();
+  if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  return (password.length < 6
+    ? res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' })
+    : next());
 };
 
 module.exports = {
