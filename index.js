44e0a518674c1a05d3c6529aba633b26ce6c1816
Guilherme Martins <guimartins_9@hotmail.com>
2022-08-17 16:32:11 -0300

    feat: 1 - Crie o endpoint GET /talker

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
-const PORT = '3000';
+const PORT = 3000;
 
-app.get('/talker', async (_req, res) => {
-  const data = await fs.readFile('/talker.json');
-  const talker = await JSON.parse(data);
+app.get('/talker', (_req, res) => {
+  const data = fs.readFileSync('talker.json');
+  const talker = JSON.parse(data);
   return (!talker
     ? res.status(HTTP_OK_STATUS).json([])
     : res.status(HTTP_OK_STATUS).json(talker));
